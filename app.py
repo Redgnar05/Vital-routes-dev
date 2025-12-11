a4dfa13f3a3b4d871dea857f8ef9973d0205a8b9
Edgar Rojo <138748167+Redgnar05@users.noreply.github.com>
2025-12-11 10:19:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,30 +2,34 @@ from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 
-from db import mongo
+from db import init_db, mongo
 from utils import usuario_logueado
 
+# Rutas
 from routes.auth_routes import init_auth_routes
 from routes.dashboard_routes import init_dashboard_routes
 
 
 def crear_app():
 
-    load_dotenv()  # Cargar claves del archivo .env
+    load_dotenv()  # Cargar variables .env
 
+    # Crear instancia de Flask
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+    # Cargar SECRET_KEY y MONGO_URI desde .env
+    app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY")
     app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
-    mongo.init_app(app)
+    # Inicializar base de datos
+    init_db(app)
 
-
+    # ======== RUTA HOME ========
     @app.route("/")
     def home():
         return render_template("index.html", logueado=usuario_logueado())
 
-
-    # Registrar rutas
+    # Registrar rutas modulares
     init_auth_routes(app)
     init_dashboard_routes(app)
 
@@ -34,4 +38,4 @@ def crear_app():
 
 if __name__ == "__main__":
     app = crear_app()
-    app.run()
+    app.run(debug=True)
